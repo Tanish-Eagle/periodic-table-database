f306d3dccf9dd0ffef30e4c5c2294b74818a8947
Tanish-Eagle <ta.shrivastava@gmail.com>
2024-05-28 22:20:26 +0530

    feat: fixed the columns in the database

---
+++ periodic_table/fix_issues.sql
@@ -0,0 +1,40 @@
+ALTER TABLE
+    properties RENAME COLUMN weight TO atomic_mass;
+
+ALTER TABLE
+    properties RENAME COLUMN melting_point TO melting_point_celsius;
+
+ALTER TABLE
+    properties RENAME COLUMN boiling_point TO boiling_point_celsius;
+
+ALTER TABLE
+    properties
+ALTER COLUMN
+    melting_point_celsius
+SET
+    NOT NULL,
+ALTER COLUMN
+    boiling_point_celsius
+SET
+    NOT NULL;
+
+ALTER TABLE
+    elements
+ADD
+    CONSTRAINT unique_symbol_name UNIQUE(symbol, name);
+
+ALTER TABLE
+    elements
+ALTER COLUMN
+    symbol
+SET
+    NOT NULL,
+ALTER COLUMN
+    name
+SET
+    NOT NULL;
+
+ALTER TABLE
+    properties
+ADD
+    FOREIGN KEY(atomic_number) REFERENCES elements(atomic_number);
